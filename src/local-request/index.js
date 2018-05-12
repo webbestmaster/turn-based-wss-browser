d222265c0f0ea012a1fab0677d3c8b6269013c94
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2018-05-12 16:48:19 +0300

    in progress...

--- src/local-request/index.js
+++ src/local-request/index.js
@@ -1,12 +1,14 @@
 // @flow
 
+/* global setTimeout */
+
 /* eslint consistent-this: ["error", "localRequest"] */
 const {localMaster} = require('./../local-master');
 
 export type RequestCallBackType = (error: Error | null, response: mixed, body: string) => void;
 
 function request(requestType: 'get' | 'post', url: string, form: mixed, requestCallBack: RequestCallBackType) {
-    localMaster.triggerHttp(requestType, url, form, requestCallBack);
+    setTimeout((): void => localMaster.triggerHttp(requestType, url, form, requestCallBack), 0);
 }
 
 function get(url: string, requestCallBack: RequestCallBackType) {
